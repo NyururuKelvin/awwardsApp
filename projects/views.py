311dd1eddb7ffb5d7ae42c3168fe10790886e359
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-24 17:34:01 +0300

    my commit add authentication views

--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,24 @@ from django.http  import HttpResponse
 def index(request):
     # Default view
     return render(request,'project/index.html')
+
+def signup(request):
+    name = "Sign Up"
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            name = form.cleaned_data.get('username')
+            send_mail(
+            'Welcome to Awwards Gallery App.',
+            f'Hello {name},\n '
+            'Welcome to Instagram App and have fun.',
+            'nyururukelvin99@gmail.com@gmail.com',
+            [email],
+            fail_silently=False,
+            )
+        return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form, 'name':name})
