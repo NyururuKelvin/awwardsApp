47448eaabc48a65e9ab1180624a46366605ff6a8
Nyururukelvin <nyururukelvin9@gmail.com>
2020-10-24 14:36:47 +0300

    my commit setup root view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
-# Create your views here.
+# Views
+def index(request):
+    # Default view
+    return HttpResponse('Welcome to Projects Gallery')
